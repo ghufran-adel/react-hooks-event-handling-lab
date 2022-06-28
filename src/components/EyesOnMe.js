dfedc7288038c403a3cda05abf4ab764b5773bbf
ghofran <gho0ofran.adel14@gmail.com>
2022-06-28 12:53:20 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+export default function EyesOnMe () {
+
+    function Onfoucs(){
+        console.log('Good!')
+    }
+
+    function onBlur(){
+        console.log('Hey! Eyes on me!')
+        
+    }
+    return(
+        <div>
+        <button onBlur={onBlur} onFocus={Onfoucs}>Eyes on me</button>
+        </div>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+
+export default function Keypad () {
+    function HandleSubmit(e) {
+       e.preventDefault();
+       console.log(`Entering password...`);
+     }
+       return(
+           <div>
+            <input 
+            type="password"  
+            name="password"
+            placeholder="Enter password"
+            onChange={HandleSubmit}
+            />
+               
+            </div>
+       )
+   }
